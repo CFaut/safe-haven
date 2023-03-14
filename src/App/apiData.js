8920bdb82f0175d1f286fffbe1d2292742ae4581
Casey <86919186+CFaut@users.noreply.github.com>
2023-03-14 12:39:36 +0200

    Fixing title

--- src/App/apiData.js
+++ src/App/apiData.js
@@ -3,13 +3,6 @@ import React from "react";
 const root = 'https://www.petrescue.com.au/api/listings?species='
 const species = 'Cat'
 
-// export const getCats = async (cat) => {
-//   const response = await fetch(`${root}/${cat}`.json);
-//   const json = await response.json();
-
-//   return json.data;
-// };
-
 export const getCats = async () => {
   const endpoint = `${root}${species}`;
   try{
